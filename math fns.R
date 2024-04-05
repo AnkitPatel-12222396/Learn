2358df22cc1dfa89d950ef3e0b4fbe4f1e4e2610
Ankit Patel <work.ankitpatel1@gmail.com>
2024-04-05 23:20:21 +0530

    Add files via upload

---
+++ math fns.R
@@ -0,0 +1,25 @@
+#math functions
+abs(-14)#return absolute value # 14
+
+sqrt(16) #4
+sqrt(-16) #error
+
+#ceiling
+ceiling(10.1)#round off to higher value #11
+ceiling(-10.1) #-10
+
+floor(10.9) #10
+floor(-10.9) #-11
+
+trunc(12.56789)# truncates decimal part #12
+
+round(12.56789) #13
+round(12.56789,digit=3) #12.568
+
+log(10) #natural logarithmic of base e  2.302585
+log10(10) #1 #log with base 10
+
+sin(30)
+cos(45)
+tan(90)
+exp(2) #exponential
