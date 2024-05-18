ea0ee405b8c380cef37df8a7cd0b569c11e4e4d4
Ankit Patel <work.ankitpatel1@gmail.com>
2024-05-18 23:19:48 +0530

    Add files via upload

---
+++ math.R
@@ -0,0 +1,25 @@
+#math function
+abs(-14)
+sqrt(16)
+sqrt(-16)
+ceiling(-10.9)
+floor(-10.9)
+ceiling(10.9)
+floor(10.9)
+trunc(12.34567)#truncates decimal part i.e. remove decimal part
+
+round(12.345)
+round(12.455)
+round(12.500)
+round(12.5001)
+round(12.50005,digits = 3)
+
+log(10)#natural logarithmic function with base e
+log10(10)# natural logarithmic function with base 10 
+log2(8)
+
+sin(30)
+cos(30)
+tan(45)
+exp(2)
+
